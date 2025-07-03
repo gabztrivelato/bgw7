b8ad5edae6ab6a77b8251833f8790044cd3c8821
gabztrivelato <gabrielletrivelatosilva@hotmail.com>
2025-07-03 11:09:57 -0300

    feat: add test tickets on desafio-web

--- go-web/desafio-encerramento/internal/service/ticket_default_test.go
+++ go-web/desafio-encerramento/internal/service/ticket_default_test.go
@@ -1,40 +1,64 @@
 package service_test
 
 import (
-	"app/internal"
 	"app/internal/repository"
+	"app/internal/service"
+	"app/loader"
+
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
+
+const testPath = "../../loader/tickets.csv"
 
 // Tests for ServiceTicketDefault.GetTotalAmountTickets
 func TestServiceTicketDefault_GetTotalAmountTickets(t *testing.T) {
 	t.Run("success to get total tickets", func(t *testing.T) {
-		// arrange
-		// - repository: mock
-		rp := repository.NewRepositoryTicketMock()
-		// - repository: set-up
-		rp.FuncGet = func() (t map[int]internal.TicketAttributes, err error) {
-			t = map[int]internal.TicketAttributes{
-				1: {
-					Name:    "John",
-					Email:   "johndoe@gmail.com",
-					Country: "USA",
-					Hour:    "10:00",
-					Price:   100,
-				},
-			}
-			return
-		}
+		loader := loader.NewLoaderTicketCSV(testPath)
+		ticket, _ := loader.Load()
+		lastId := len(ticket)
 
-		// - service
-		// sv := service.NewServiceTicketDefault(rp)
+		rp := repository.NewRepositoryTicketMap(ticket, lastId)
+		sv := service.NewServiceTicketDefault(*rp)
+		total, err := sv.GetTotalAmountTickets()
 
-		// // act
-		// total, err := sv.GetTotalTickets()
+		expectedTotal := 1000
+		require.NoError(t, err)
+		require.Equal(t, expectedTotal, total)
+	})
+}
 
-		// // assert
-		// expectedTotal := 1
-		// require.NoError(t, err)
-		// require.Equal(t, expectedTotal, total)
+// Tests for TestServiceTicketDefault.GetTicketByDestinationCountry
+func TestServiceTicketDefault_GetTicketByDestinationCountry(t *testing.T) {
+	t.Run("success to get by destination", func(t *testing.T) {
+		loader := loader.NewLoaderTicketCSV(testPath)
+		ticket, _ := loader.Load()
+		lastId := len(ticket)
+
+		rp := repository.NewRepositoryTicketMap(ticket, lastId)
+		sv := service.NewServiceTicketDefault(*rp)
+		total, err := sv.GetTicketByDestinationCountry("Brazil")
+
+		expectedTotal := 45
+		require.NoError(t, err)
+		require.Equal(t, expectedTotal, total)
+	})
+}
+
+// Tests for TestServiceTicketDefault.GetPercentageTicketsByDestinationCountry
+func TestServiceTicketDefault_GetPercentageTicketsByDestinationCountry(t *testing.T) {
+	t.Run("success to get percentage by destination country", func(t *testing.T) {
+		loader := loader.NewLoaderTicketCSV(testPath)
+		ticket, _ := loader.Load()
+		lastId := len(ticket)
+
+		rp := repository.NewRepositoryTicketMap(ticket, lastId)
+		sv := service.NewServiceTicketDefault(*rp)
+		total, err := sv.GetPercentageTicketsByDestinationCountry("Brazil")
+
+		expectedTotal := 4.5
+		require.NoError(t, err)
+		require.Equal(t, expectedTotal, total)
 	})
 }
